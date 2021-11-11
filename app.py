7b0ad4abee01cf249ad09703061180eff3cb8398
Shirude Rhuchita <56022804+Rhuchita@users.noreply.github.com>
2021-11-11 12:07:28 +0530

    Color Palette updated

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, flash, request, redirect, url_for
 import legofy
 from PIL import Image
+import webcolors
+from colorthief import ColorThief
 #import os
 
 app = Flask(__name__)
 
 
-def color_palette(mode):
-    if mode == "solid":
-        return ("#fec400", "#e76318", "#de000d", "#de378b", "#0057a8", "#ffff99", "#ee9dc3", "#87c0ea", "#f49b00", "#9c006b", "#478cc6",
-                "#5e748c", "#80081b", "#2c1577", "#002541", "#007b28", "#009624", "#5f8265", "#003416", "#95b90b", "#5b1c0c", "#d9bb7b",
-                "#8d7452", "#aa7d55", "#300f06", "#d67240", "#f5c189", "#a83d15", "#f4f4f4", "#e4e4da", "#9c9291", "#4c5156", "#010101")
-    elif mode == "transparent":
-        return ("#f9ef69", "#ec760e", "#e76648", "#e02a29", "#ee9dc3", "#9c95c7", "#b6e0ea",
-                "#50b1e8", "#cee3f6", "#63b26e", "#99ff66", "#faed5b", "#a69182", "#eeeeee")
-    elif mode == "effects":
-        return ("#8d9496", "#aa7f2e", "#493f3b", "#fefcd5")
-    else:
-        return "1,2,3,4,5,6,7"
+def color_palette(image_path):
+    image_colors = []
+    with open(image_path, 'r+b') as f:
+        with Image.open(f) as image:
+            color_thief = ColorThief(image_path)
+            color_palette = color_thief.get_palette(
+                color_count=10, quality=10)
+            for color in color_palette:
+                image_colors.append(webcolors.rgb_to_hex(color))
+    return image_colors
 
 
 @app.route('/')
@@ -49,8 +49,9 @@ def uploader():
         legos, legoext = map(str, legoname.split("."))
         legofn = legos+"_lego.png"
         legopath = "uploads/"+legofn
+        img_path = "static/"+legopath
 
-        selected_palette = color_palette(palettes)
+        selected_palette = color_palette(img_path)
         return render_template('index.html', results=selected_palette, p_mode=palettes, imagepath=legopath)
 
 
